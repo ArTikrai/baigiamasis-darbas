57b9c3ba9db0fd6691b81052da39053228363279
Vytautas <vytautas.palubinskass@gmail.com>
2021-12-21 21:34:19 +0200

    upgrade2

---
+++ script.js
@@ -0,0 +1,80 @@
+//
+// About section tab navigation
+//
+
+const tabNavigationElements = document.querySelectorAll(
+  ".about .tab-navigation li"
+);
+const tabContentElements = document.querySelectorAll(".about .tab-content");
+
+function removeActiveClassesFromElements(elements) {
+  for (let element of elements) {
+    element.classList.remove("active");
+  }
+}
+
+for (let tabElement of tabNavigationElements) {
+  tabElement.addEventListener("click", function () {
+    const target = this.dataset.target; // "register" / "apply" / "receive"
+
+    removeActiveClassesFromElements(tabNavigationElements);
+    this.classList.add("active");
+
+    removeActiveClassesFromElements(tabContentElements);
+    document
+      .querySelector('.tab-content[data-tab="' + target + '"]')
+      .classList.add("active");
+  });
+}
+
+//
+// Reviews section swiper gallery
+//
+
+const swiper = new Swiper(".reviews-swiper", {
+  speed: 900,
+  slidesPerView: 1,
+  spaceBetween: 50,
+  // slidesPerGroup: 3,
+  autoHeight: true,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 120,
+    },
+  },
+});
+
+//
+// Get weather data from openweather API
+//
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Vilnius&units=metric&appid=311b9d4d0f48a2eaec9e0024d33228e1";
+const weatherElement = document.getElementById("weather-in-celsius");
+
+function getCurrentWeatherInCelsius() {
+  const http = new XMLHttpRequest();
+  http.open("GET", url);
+  http.addEventListener("load", function () {
+    const response = JSON.parse(http.response);
+    const temperature = Math.round(response.main.temp);
+
+    if (temperature > 0) {
+      weatherElement.innerText = "+" + temperature;
+    } else {
+      weatherElement.innerText = temperature;
+    }
+  });
+  http.send();
+}
+
+window.addEventListener("load", getCurrentWeatherInCelsius);
